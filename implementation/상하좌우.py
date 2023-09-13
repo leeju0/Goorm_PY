e2c9036c488949a2a3890a231c362888b5b153f9
leeju0 <92801522+leeju0@users.noreply.github.com>
2023-09-14 01:13:02 +0900

    오늘도 한문제 . . .

--- implementation/상하좌우.py
+++ implementation/상하좌우.py
@@ -1,24 +1,23 @@
 N = int(input())
 plans = input().split()
 
-dy = [0, -1, 0, 1]
-dx = [-1, 0, 1, 0]
+x, y = 1,1 #시작 좌표
 
-# 이동 타입
-move = ['U', 'L', 'D', 'R']
+dy = [0,-1,0,1]
+dx = [-1,0,1,0]
 
-# 시작 좌표
-x, y = 1, 1
+site = ['U','L','D','R']
 
 for plan in plans:
-    for i in range(4):
-        if plan == move[i]:  # 만약 일치하면 -> 이동함
-            ny = y + dy[i]  # 원래 y값에서 해당 동작에 따라 왼, 위, 아래, 오 중에 이동함 이동한 좌표가 ny
+    for i in range(len(dy)):
+        if plan == site[i]:
             nx = x + dx[i]
 
-            if ny < 1 or ny > N or nx < 1 or nx > N:  # nx, ny 사용 시 예외 조건
+            ny = y + dy[i]
+
+            if nx <1 or ny <1 or nx >N or ny >N: #배열 크기를 벗어나는 경우 예외처리
                 continue
 
-            x, y = nx, ny  # 바뀐 위치 대입
+            x,y = nx, ny #바뀐 위치 대입
 
-print(x, y)
+print(x,y)
